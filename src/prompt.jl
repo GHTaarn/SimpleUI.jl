3eba3065d490247764b4d32be1be661933f88548
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2024-06-07 06:47:08 +0200

    Fixed bug in prompt()

--- src/prompt.jl
+++ src/prompt.jl
@@ -6,7 +6,7 @@ If the user inputs the empty string and `default != nothing` then `default` is
 returned.
 """
 function prompt(promptstr::AbstractString, default::Union{AbstractString,Nothing}=nothing)
-    default == nothing ? print("$prompstr: ") : print("$promptstr ($default): ")
+    default == nothing ? print("$promptstr: ") : print("$promptstr ($default): ")
     input = readline()
     isempty(input) && !isnothing(default) ? default : input
 end
