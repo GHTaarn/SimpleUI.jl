53b30def956dd31b695fd10b0f9f27c342e2cdb1
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2024-08-28 18:36:53 +0200

    Unit test for pause()

--- test/pause.jl
+++ test/pause.jl
@@ -1,3 +1,10 @@
 @testset "pause" begin
+    open("data/pause.txt","r") do stream
+        redirect_stdin(stream) do
+            @test readline() == "abc"
+            pause()
+            @test readline() == "def"
+        end
+    end
 end
 
