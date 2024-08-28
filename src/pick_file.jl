5280e23000a00170288ffff91802417ec19aeef1
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2024-08-28 05:04:25 +0200

    Enhanced pick_file()

--- src/pick_file.jl
+++ src/pick_file.jl
@@ -1,19 +1,26 @@
 using REPL.TerminalMenus: request, RadioMenu
 
 """
-    pick_file(dir="."; pattern=r"", join=true)
+    pick_file(startdir="."; pattern=r"")
 
-Show the files in `dir` that match `pattern` and return the one chosen by the
-user. Immediately returns `nothing` if there are no files to choose from.
-
-The `join` keyword works like in the Julia `readdir` function, i.e. if set to
-`false` then the raw filename is returned, if set to `true` then `pick_file`
-returns`joinpath(dir, fname)` where `fname` is the filename selected by the
-user.
+Allow the user to browse through files matching `pattern` in the file system
+and return the path of the file selected by the user.
 """
-function pick_file(dir="."; pattern=r"", join=true)
-    files=filter(contains(pattern), readdir(dir))
+function pick_file(dir="."; pattern=r"")
+    files = [isdir(joinpath(dir,s)) ? s*"/" : s for s in readdir(dir)]
+    files = filter(files) do file
+        contains(pattern)(file) || contains(r"/$")(file)
+    end
+    if isdir(joinpath(dir,".."))
+        files = vcat("../", files)
+    end
     length(files) == 0 && return nothing
+    println("\nSelect a file from $dir:")
     fname = files[request(RadioMenu(files))]
-    join ? joinpath(dir, fname) : fname
+    fullpath = joinpath(dir, fname)
+    if fullpath[end] == '/'
+        pick_file(normpath(fullpath[1:end-1]); pattern)
+    else
+        fullpath |> normpath
+    end
 end
