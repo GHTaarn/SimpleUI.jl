7bab4de66baa27b5acf58c05b020320d8b3be083
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2024-08-28 18:27:09 +0200

    Unit test for promptget()

--- test/promptget.jl
+++ test/promptget.jl
@@ -1,3 +1,28 @@
 @testset "promptget" begin
+    using Dates: Date
+    open("data/promptget.txt","r") do stream
+        redirect_stdin(stream) do
+            @test promptget("Give me a String") == "abcሴ"
+            println()
+            @test promptget(Date, "Give me a Date") == Date(2024, 12, 31)
+            println()
+            @test promptget("Give me a Float64", 1.0) === 1.0
+            println()
+            @test promptget("Give me a Float64", NaN) === NaN
+            println()
+            @test promptget("Give me a Float64", Inf) === Inf
+            println()
+            @test promptget("Give me a Float64", 1.0) === 2.0
+            println()
+            @test promptget(Float64, "Give me a Float64", 1000) == 1000.0
+            println()
+            @test promptget(Float64, "Give me a Float64", pi) === pi
+            println()
+            @test promptget(Float64, "Give me a Float64", 1) === 2.0
+            println()
+            @test_throws Exception promptget(Float64, "Give me a Float64")
+            println()
+        end
+    end
 end
 
