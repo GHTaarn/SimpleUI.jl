5dc09657043914bf511c2e05fa1045cd7ef75f72
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2025-11-08 18:42:02 +0100

    Improved pick_file()
    Allow no file to be picked
    Allow kwargs to be passed on to TerminalMenus.RadioMenu
    Corrected the docstring

--- src/pick_file.jl
+++ src/pick_file.jl
@@ -1,10 +1,14 @@
 using REPL.TerminalMenus: request, RadioMenu
 
 """
-    pick_file(startdir="."; pattern=r"")
+    pick_file(startdir="."; pattern=r"", kwargs...)
 
 Allow the user to browse through files matching `pattern` in the file system
-and return the path of the file selected by the user.
+and return the path of the file selected by the user. If the user presses 'q'
+then `nothing` is returned.
+
+This function uses `TerminalMenus.RadioMenu` from the `REPL` standard library
+and `kwargs` is passed on to that function.
 """
 function pick_file(dir="."; pattern=r"")
     files = [isdir(joinpath(dir,s)) ? s*"/" : s for s in readdir(dir)]
@@ -16,10 +20,12 @@ function pick_file(dir="."; pattern=r"")
     end
     length(files) == 0 && return nothing
     println("\nSelect a file from $dir:")
-    fname = files[request(RadioMenu(files))]
+    isel = request(RadioMenu(files; kwargs...))
+    isel <= 0 && return nothing
+    fname = files[isel]
     fullpath = joinpath(dir, fname)
     if fullpath[end] == '/'
-        pick_file(normpath(fullpath[1:end-1]); pattern)
+        pick_file(normpath(fullpath[1:end-1]); pattern, kwargs...)
     else
         fullpath |> normpath
     end
